41b3d0125570992ed918bc6236a1099d5d6a8704
Jun_Lei <v-jlei@hotels.com>
2021-02-01 00:33:40 +0800

    optimize and refactor testcases

--- testcases/test_calculator.py
+++ testcases/test_calculator.py
@@ -14,14 +14,23 @@ sys.path.append('..')
 from pythoncode.Calculator import Calculator
 
 
-def get_datas():
-    with open("./testcases/datas/calc.yml") as f:
-        datas = yaml.safe_load(f)
-        return datas
+def get_datas(name, type='int'):
+    with open("./datas/calc.yml") as f:
+        all_datas = yaml.safe_load(f)
+        print(all_datas)
+    datas = all_datas[name][type]['datas']
+    print(datas)
+    ids = all_datas[name][type]['ids']
+    print(ids)
+    return datas, ids
 
 
 class TestCalc:
-    datas: list = get_datas()
+    # datas: list = get_datas()
+    add_int_data = get_datas('add', 'int')
+    add_float_data = get_datas('add', 'float')
+    div_int_normal_data = get_datas('div', 'int_normal')
+    div_int_error_data = get_datas('div', 'int_error')
 
     def setup_class(self):
         print("Start Calculate...")
@@ -37,27 +46,36 @@ class TestCalc:
         print("--Calculation case complete--")
 
     @pytest.mark.add
-    @pytest.mark.parametrize("a, b, expected", datas["add"]["datas"], ids=datas["add"]["ids"])
+    @pytest.mark.parametrize("a, b, expected", add_int_data[0], ids=add_int_data[1])
     def test_add(self, a, b, expected):
         print(f"a={a}, b={b}, expected result={expected}")
         actual_result = self.calc.add(a, b)
-        assert expected == actual_result
+        assert actual_result == expected
 
-    # done: complete add function
-    # done: div function
-    # done: main function
-    @pytest.mark.div
-    @pytest.mark.parametrize("a, b, expected", datas["div"]["datas"], ids=datas["div"]["ids"])
-    def test_div(self, a, b, expected):
+    @pytest.mark.add
+    @pytest.mark.parametrize("a, b, expected", add_float_data[0], ids=add_float_data[1])
+    def test_add_float(self, a, b, expected):
         print(f"a={a}, b={b}, expected result={expected}")
-        if b != 0:
-            actual_result = self.calc.div(a, b)
-            assert expected == actual_result
-        else:
-            try:
-                self.calc.div(a, b)
-            except Exception as e:
-                print(e)
+        actual_result = round(self.calc.add(a, b), 8)
+        assert actual_result == expected
+
+    @pytest.mark.div
+    @pytest.mark.parametrize("a, b, expected", div_int_normal_data[0], ids=div_int_normal_data[1])
+    def test_div_normal(self, a, b, expected):
+        print(f"a={a}, b={b}, expected result={expected}")
+        actual_result = self.calc.div(a, b)
+        assert actual_result == expected
+
+    @pytest.mark.div
+    @pytest.mark.parametrize("a, b, expected", div_int_error_data[0], ids=div_int_error_data[1])
+    def test_div_error(self, a, b, expected):
+        print(f"a={a}, b={b}, expected result={expected}")
+        with pytest.raises(ZeroDivisionError):  # pytest inner function
+            self.calc.div(a, b)
+        # try:
+        #     self.calc.div(a, b)
+        # except Exception as e:
+        #     print(e)
 
 
 if __name__ == '__main__':
