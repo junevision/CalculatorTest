ea8760788aadd096b79bb8bd65f29794f0efeda9
Jun_Lei <v-jlei@hotels.com>
2021-02-02 11:35:10 +0800

    refacter get_data code to conftest file, activate addopts settings and generate allure report

---
+++ testcases/conftest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Mingjun Lei
+@file: conftest.py.py
+@time: 2021/2/2 11:12
+@desc: get test data from yaml
+"""
+import pytest
+import yaml
+
+
+def get_datas(name, type='int'):
+    with open("./datas/calc.yml") as f:
+        all_datas = yaml.safe_load(f)
+    datas = all_datas[name][type]['datas']
+    ids = all_datas[name][type]['ids']
+    return datas, ids
+
+
+@pytest.fixture(params=get_datas('add', 'int')[0], ids=get_datas('add', 'int')[1])
+def get_add_int_datas_with_fixture(request):
+    return request.param
+
+
+@pytest.fixture(params=get_datas('add', 'float')[0], ids=get_datas('add', 'float')[1])
+def get_add_float_datas_with_fixture(request):
+    return request.param
+
+
+@pytest.fixture(params=get_datas('div', 'int_normal')[0], ids=get_datas('div', 'int_normal')[1])
+def get_div_int_normal_datas_with_fixture(request):
+    return request.param
+
+
+@pytest.fixture(params=get_datas('div', 'int_error')[0], ids=get_datas('div', 'int_error')[1])
+def get_div_int_error_datas_with_fixture(request):
+    return request.param
--- testcases/test_calculator.py
+++ testcases/test_calculator.py
@@ -8,38 +8,9 @@
 """
 import sys
 import pytest
-import yaml
 
 sys.path.append('..')
 from pythoncode.Calculator import Calculator
-
-
-def get_datas(name, type='int'):
-    with open("./datas/calc.yml") as f:
-        all_datas = yaml.safe_load(f)
-    datas = all_datas[name][type]['datas']
-    ids = all_datas[name][type]['ids']
-    return datas, ids
-
-
-@pytest.fixture(params=get_datas('add', 'int')[0], ids=get_datas('add', 'int')[1])
-def get_add_int_datas_with_fixture(request):
-    return request.param
-
-
-@pytest.fixture(params=get_datas('add', 'float')[0], ids=get_datas('add', 'float')[1])
-def get_add_float_datas_with_fixture(request):
-    return request.param
-
-
-@pytest.fixture(params=get_datas('div', 'int_normal')[0], ids=get_datas('div', 'int_normal')[1])
-def get_div_int_normal_datas_with_fixture(request):
-    return request.param
-
-
-@pytest.fixture(params=get_datas('div', 'int_error')[0], ids=get_datas('div', 'int_error')[1])
-def get_div_int_error_datas_with_fixture(request):
-    return request.param
 
 
 @pytest.fixture()
